4541a839889f44a3b2e37047ab209c0c5411ba11
s1mpl3x <simplex21@googlemail.com>
2011-07-04 18:03:58 +0200

    added EntityListener & Arrays for Worlds&Players

--- src/me/simplex/buildr/Buildr.java
+++ src/me/simplex/buildr/Buildr.java
@@ -1,7 +1,15 @@
 package me.simplex.buildr;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Type;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,12 +18,19 @@ import com.nijiko.permissions.PermissionHandler;
 import com.nijikokun.bukkit.Permissions.Permissions;
 
 public class Buildr extends JavaPlugin {
+	
+	  //tech
 	  public static PermissionHandler permissionHandler;
+	  public Buildr_EntityListener EntityListener;
 	  public Logger log = Logger.getLogger("Minecraft");
 	  private String prefix;
 	  private String version;
 	  private String pluginDirectory;
 	  private PluginManager pm;
+	  
+	  //logic
+	  private ArrayList<World> worldbuildmode;
+	  private ArrayList<Player> playerbuildmode;
 
 	@Override
 	public void onDisable() {
@@ -26,15 +41,29 @@ public class Buildr extends JavaPlugin {
 	public void onEnable() {
 		//init
 		 pm = getServer().getPluginManager();
+		 EntityListener = new Buildr_EntityListener(this);
 		 version = getDescription().getVersion();
 		 prefix = "[Buildr] ";
+		 
+		 
+		 worldbuildmode =  new ArrayList<World>();
+		 playerbuildmode = new ArrayList<Player>();
+		 
+		 //load
 		 log("Buildr v"+version+" loading..");
 		 setupPermissions();
 		 
 		 //register Listener
 		 //TODO
-		 
-		
+		 pm.registerEvent(Type.ENTITY_DAMAGE, EntityListener, Event.Priority.Normal, this);
+		 pm.registerEvent(Type.ENTITY_TARGET, EntityListener, Event.Priority.Normal, this);
+		 pm.registerEvent(Type.ITEM_SPAWN, EntityListener, Event.Priority.Normal, this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,String label, String[] args) {
+		// TODO Auto-generated method stub
+		return super.onCommand(sender, command, label, args);
 	}
 	
 	private void setupPermissions() {
@@ -55,6 +84,13 @@ public class Buildr extends JavaPlugin {
 	
 	protected void log(String msg){
 		log.info(prefix+msg);
+	}
+	
+	protected boolean checkWorldBuildMode(World world){
+		return worldbuildmode.contains(world);
+	}
+	protected boolean checkPlayerBuildMode(Player player){
+		return playerbuildmode.contains(player);
 	}
 
 }
---
+++ src/me/simplex/buildr/Buildr_EntityListener.java
@@ -0,0 +1,43 @@
+package me.simplex.buildr;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityListener;
+import org.bukkit.event.entity.EntityTargetEvent;
+import org.bukkit.event.entity.ItemSpawnEvent;
+
+public class Buildr_EntityListener extends EntityListener {
+	private Buildr plugin;
+	
+	public Buildr_EntityListener(Buildr plugin) {
+		this.plugin = plugin;
+	}
+	
+// Godmode: No Dmg
+@Override
+public void onEntityDamage(EntityDamageEvent event) {
+	if (event.getEntity() instanceof Player) {
+		if (plugin.checkPlayerBuildMode((Player)event.getEntity())) {
+			event.setCancelled(true);
+		}
+	}
+}
+
+//Globalbuildmode: No Drops
+@Override
+	public void onItemSpawn(ItemSpawnEvent event) {
+		if (plugin.checkWorldBuildMode(event.getLocation().getWorld())) {
+			event.setCancelled(true);
+		}
+	}
+
+//Godmode: No Target
+@Override
+	public void onEntityTarget(EntityTargetEvent event) {
+		if (event.getTarget() instanceof Player) {
+			if (plugin.checkPlayerBuildMode((Player)event.getEntity())) {
+				event.setCancelled(true);
+			}
+		}
+	}
+}
