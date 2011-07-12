88784d8d26b13d244413487e022a7ce0b8736be6
s1mpl3x <simplex21@googlemail.com>
2011-07-13 00:24:51 +0200

    finished wallx

--- src/me/simplex/buildr/runnable/Buildr_Runnable_Builder_Wallx.java
+++ src/me/simplex/buildr/runnable/Buildr_Runnable_Builder_Wallx.java
@@ -34,62 +34,60 @@ public class Buildr_Runnable_Builder_Wallx implements Runnable {
 		
 		plugin.getUndoList().addToStack(undoBlocks, player);
 		player.sendMessage("done! Placed "+undoBlocks.size()+" blocks");
-		plugin.log(player.getName()+" builded a wall: "+undoBlocks.size()+" blocks affected");
+		plugin.log(player.getName()+" builded a wallx: "+undoBlocks.size()+" blocks affected");
 	}
 	
 	private HashMap<Block, Buildr_Container_UndoBlock> buildWall(){
 		ArrayList<Block> groundblocks = new ArrayList<Block>();
-		ArrayList<Block> toBuild = new ArrayList<Block>();
 		HashMap<Block, Buildr_Container_UndoBlock> undo = new HashMap<Block, Buildr_Container_UndoBlock>();
 		
 		int height 		= calcDistance(position1.getY(), position2.getY());
 		int level  		= calcStartPoint(position1.getY(), position2.getY());
 		
-		int pos1_x		= position1.getX();
-		int pos2_x		= position2.getX();
+		int side_a 		= calcDistance(position1.getX(), position2.getX());
+		int side_b 		= calcDistance(position1.getZ(), position2.getZ());
 		
-		int pos1_z		= position1.getZ();
-		int pos2_z		= position2.getZ();
+		double pos1_x	= position1.getX();
+		double pos2_x	= position2.getX();
 		
-		int side_a 		= calcDistance(pos1_x, pos2_x);
-		int side_b 		= calcDistance(pos1_z, pos2_z);
+		double pos1_z	= position1.getZ();
+		double pos2_z	= position2.getZ();
 		
 		double lenght 	= Math.sqrt((side_a*side_a)+(side_b*side_b));
 		double mod 		= 1/lenght;
-		double pos_x	= 0;
-		double pos_z;
 		double i		= 0;
 		
+		double pos_x,pos_z;
 		
 		while(i<=1){
-			
 			pos_x = pos1_x + i*(pos2_x-pos1_x);
 			pos_z = pos1_z + i*(pos2_z-pos1_z);
+			
+			pos_x = Math.round(pos_x);
+			pos_z = Math.round(pos_z);
 			
 			Location loc = new Location(player.getWorld(), pos_x, level, pos_z);
 			
 			Block block_handle = player.getWorld().getBlockAt(loc);
+
 			
 			if (!groundblocks.contains(block_handle)) {
 				groundblocks.add(block_handle);
 				for (int j = 0; j < height; j++) {
 					if (aironly) {
 						if (block_handle.getType().equals(Material.AIR)) {
-							toBuild.add(block_handle.getRelative(0, j, 0));
+							undo.put(block_handle, new Buildr_Container_UndoBlock(block_handle.getType(), block_handle.getData()));
+							block_handle.setType(material);
 						}
 					}
 					else {
-						toBuild.add(block_handle.getRelative(0, j, 0));
+						undo.put(block_handle, new Buildr_Container_UndoBlock(block_handle.getType(), block_handle.getData()));
+						block_handle.setType(material);
 					}
 					
 				}
 			}
 			i=i+(mod);
-		}
-		
-		for (Block block : toBuild) {
-			undo.put(block, new Buildr_Container_UndoBlock(block.getType(), block.getData()));
-			block.setType(material);
 		}
 		return undo;
 	}
